548ea88f905340f05716836d8d9270e45f73b1d5
Samanta Fluture <samantafluture@gmail.com>
2022-09-09 11:21:47 -0400

    create and style button submit

--- src/components/NewTask.tsx
+++ src/components/NewTask.tsx
@@ -1,10 +1,15 @@
+import { PlusCircle } from 'phosphor-react';
+
 import styles from './NewTask.module.css';
 
 export function NewTask() {
 	return (
 		<form className={styles.newTask}>
             <input type="text"placeholder='Add a new task' />
-            <button type="submit">Create</button>
+            <button type="submit">
+                <span>Create</span>
+                <PlusCircle size={16}/>
+            </button>
         </form>
 	);
 }
