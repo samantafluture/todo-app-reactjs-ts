11deb51db2b179ca08ee205dfdd030794126f71c
Samanta Fluture <samantafluture@gmail.com>
2022-09-11 15:58:42 -0400

    create task component and styles

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,14 +1,15 @@
-import { Trash, Check } from 'phosphor-react';
+import { Trash } from 'phosphor-react';
 
 import styles from './Task.module.css';
 
 export function Task() {
 	return (
-			<div className={styles.task}>
-                <input type="checkbox" className={styles.taskCheckbox} />
-				<article className={styles.taskContent}>Task</article>
-                <Trash size={16} weight="bold" />
-			</div>
-            
+		<div className={styles.task}>
+			<input type='checkbox' title='Check task' />
+			<article className={styles.taskContent}>Task</article>
+			<button type='button' title='Delete task' className={styles.deleteTask}>
+				<Trash size={16} />
+			</button>
+		</div>
 	);
 }
